c402e6edb90dbe9c9add87972ce4540575239643
Eugene Korneev <ugn@localhost.localdomain>
2016-03-09 23:30:22 +0300

    2

--- uvk.go
+++ uvk.go
@@ -1,5 +1,23 @@
 package main
 
+// https://oauth.vk.com/authorize
+// ?client_id=5331338
+//
+// &scope=notify,friends,photos,audio,video,docs,notes,pages,status,
+// wall,
+// groups,email,notifications,
+// nohttps
+// offline,messages
+//
+// &redirect_uri=https%3A%2F%2Foauth.vk.com%2Fblank.html //https://oauth.vk.com/blank.html
+// &display=page //mobile
+// &v=5.45
+// &response_type=token
+// &state=test
+
+
+
+
 import "fmt"
 
 func main() {
